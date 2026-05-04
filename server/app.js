6fc27587ad0bb6540930ca5570300a54a5ab8858
Vinod-Rajapaksha <165497570+Vinod-Rajapaksha@users.noreply.github.com>
2026-05-04 10:47:09 +0530

    feat(server): increase body size limit

--- server/app.js
+++ server/app.js
@@ -34,8 +34,8 @@ const inventoryLogsRouter = require("./routes/inventoryLogs.route");
 const app = express();
 connectDB();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
